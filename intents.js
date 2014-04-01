8530d414b07342a3ad7319490258a24124a42f16
Daniel Lamando <dan@danopia.net>
2014-03-31 19:34:04 -0700

    Add an association command for accounts

--- intents.js
+++ intents.js
@@ -1,3 +1,25 @@
+var mongo = require('mongodb');
+
 exports.weather = function (context, entities) {
   exports.reply(context, "It's sunny, bitch.", 'sunny');
 };
+
+mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db) {
+  console.log('MongoLAB connected:', error);
+  db.collection('users', function (err, users) {
+
+    exports.set_service_username = function (context, entities) {
+      var doc = {};
+      doc[entities.service.value] = entities.username.value;
+
+      users.update({user_id: context.user_id}, doc, {upsert: true}, function (err, item) {
+        if (err) {
+          reply(context, 'Error saving to mongo');
+        } else {
+          reply(context, 'I gotcha!');
+        }
+      });
+    };
+
+  });
+});
